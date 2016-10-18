9424b0953d751347e7d8c42296f579d1cf911dce
Karthik V <karthikvenkatraman@gmail.com>
2016-10-18 11:30:59 +0100

    New Exception type for Connection timeout
    New Exception type for XLR Server Connection timeout when Jira is unable to reach XLRelease server

---
+++ src/main/java/com/xebialabs/jira/xlr/client/ServerNotAvailableException.java
@@ -0,0 +1,7 @@
+package com.xebialabs.jira.xlr.client;
+
+public class ServerNotAvailableException extends XLReleaseClientException {
+    public ServerNotAvailableException(final String message) {
+        super(message);
+    }
+}
